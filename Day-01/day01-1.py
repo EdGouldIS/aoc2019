a54bda0862beb236407e399fcdf83eb2631651a4
EdGouldIS <edwardgould@informed.com>
2019-12-07 22:12:48 +0000

    Added Day 1 solutions

--- Day-01/day01-1.py
+++ Day-01/day01-1.py
@@ -1,5 +1,16 @@
 def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+    total = 0
+    for mass in puzzle_input:
+        total += int(mass) // 3 - 2
 
-    return str(solution)
+    return total
+
+
+def get_input(filename):
+    with open(filename) as f:
+        return f.read().split()
+
+
+if __name__ == '__main__':
+    puzzle_input = get_input('input.txt')
+    print(main(puzzle_input))
--- Day-01/day01-2.py
+++ Day-01/day01-2.py
@@ -1,5 +1,20 @@
 def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+    total = 0
+    for mass in puzzle_input:
+        mass = int(mass)
+        while (mass > 5): # While the mass needs more fuel add more mass
+            mass = mass // 3 - 2
+            print(mass)
+            total += mass
 
-    return str(solution)
+    return total
+
+
+def get_input(filename):
+    with open(filename) as f:
+        return f.read().split()
+
+
+if __name__ == '__main__':
+    puzzle_input = get_input('input.txt')
+    print(main(puzzle_input))

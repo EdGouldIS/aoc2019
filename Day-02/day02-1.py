d356ac16e0fb423e214bf7741cc3220efb13d54b
EdGouldIS <edwardgould@informed.com>
2019-12-09 20:01:23 +0000

    Added day 2 part 1

--- Day-02/day02-1.py
+++ Day-02/day02-1.py
@@ -1,5 +1,28 @@
 def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+    memory = [int(_) for _ in puzzle_input.split(',')]
+    memory[1] = 12
+    memory[2] = 2
 
-    return str(solution)
+    for i in range(0, len(memory), 4):
+        operation = memory[i]
+        if (operation == 1):
+            memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
+        
+        elif (operation == 2):
+            memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
+
+        else:
+            return memory[0]
+
+    return memory[0]
+
+
+def get_input(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    puzzle_input = get_input('input.txt')
+    main(puzzle_input))
+    
